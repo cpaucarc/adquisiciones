f5361ba436d89782ccdba5f0c809096c711075c0
Frank <fcpaucar@gmail.com>
2021-06-13 11:13:35 -0500

    initial setup

---
+++ resources/views/testview.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<h1>Titulo del documento</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium beatae consectetur fugit incidunt quam,
+    quibusdam reiciendis sunt voluptates voluptatum! Consectetur ducimus eius fuga iste nesciunt possimus sit tempora
+    ut!
+</p>
+</body>
+</html>
